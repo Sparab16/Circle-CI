adb2254d3f2d15b09da9e331605f17cd7e958ffb
Shreyas Parab <shreyas.parab16@gmail.com>
2021-11-27 10:12:42 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def index():
+    message = 'Circle CI Demonstation'
+    return "<h1>{}</h1>".format(message)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=5001)
+    
