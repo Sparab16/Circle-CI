2df35a9fdbc65affff6b47296c359ea9f4c4d2d9
Shreyas Parab <shreyas.parab16@gmail.com>
2021-11-27 12:44:12 +0530

    Made code changes to handle the port

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from wsgiref import simple_server
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,7 +9,10 @@ def index():
     message = 'Deployed to Heroku using Circle CI'
     return "<h1>{}</h1>".format(message)
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
-    
+port = int(os.getenv("PORT", 5000))
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    # port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
