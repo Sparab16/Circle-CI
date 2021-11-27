23042fa6a9031a0e8cb291ccafef09ea35cf844e
Shreyas Parab <shreyas.parab16@gmail.com>
2021-11-27 10:16:13 +0530

    Second commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def index():
-    message = 'Circle CI Demonstation'
+    message = 'Circle CI Demonstation number 2'
     return "<h1>{}</h1>".format(message)
 
 if __name__ == '__main__':
